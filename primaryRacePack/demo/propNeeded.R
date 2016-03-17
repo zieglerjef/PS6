7657646ca1939a0d11fed4b69b42938b46242bb4
zieglerjef <zieglerjef@gmail.com>
2016-03-17 16:35:26 -0500

    Add demo(propNeeded.R) file

---
+++ primaryRacePack/demo/propNeeded.R
@@ -0,0 +1,6 @@
+# execute propNeeded function with newCandidate object of class Candidate
+# default remaining delegates as of March, 17 (1396)
+propNeeded(newCandidate)
+
+# user specified remaining delegates
+propNeeded(newCandidate, 1396) 
