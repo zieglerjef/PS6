6b075213488b61f5578fb29c487a3835e72650b7
zieglerjef <zieglerjef@gmail.com>
2016-03-17 16:35:38 -0500

    Add primaryRacePack-package.R file

---
+++ primaryRacePack/primaryRacePack-package.r
@@ -0,0 +1,20 @@
+#' primaryRacePack
+#'
+#' The primaryRacePack package calculates the number of delegates needed based on the number of delegates won and remaining.  
+#' @name primaryRacePack
+#' @docType package
+#' @author  Jeff Ziegler: \email{jeffreyziegler@@wustl.edu} and 
+#' @examples 
+#' 
+#' newCandidate <- createCandidate("Ben Carson", 8, "Republican")
+#' str(newCandidate)
+#' propNeeded(newCandidate, 1396)
+#' 
+#' @seealso \code{\link{createCandidate}}, \code{\link{Candidate}}, \code{\link{propNeeded}}
+#'
+#' \dontrun{
+#' demo(createCandidate.R)
+#' demo(propNeeded.R)
+#' }
+#'
+NULL
