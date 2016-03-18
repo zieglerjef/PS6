65d9a57e7e15de83b8c03ae6176feced54d3e2b1
zieglerjef <zieglerjef@gmail.com>
2016-03-18 11:46:59 -0500

    Update DevelopmentFile.R

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -1,11 +1,11 @@
 ## Load libraries and set working directory
 library(devtools)
 library(roxygen2)
-setwd("~/Documents/Git/Class/PS6/primaryRacePack-package")
+setwd("~/Google Drive/Washu/Spring2016/appliedStats/problemSets/PS6/primaryRacePack-package")
 
 # create package skelton so it can find the package root
-package.skeleton(name="primaryRacePack")
-
+package.skeleton(code_files = c("primaryRacePack-package.R", "Candidate.R", "createCandidate.R", "propNeeded.R"),
+                 name="primaryRacePack")
 
 # Run to create package
 current.code <- as.package("primaryRacePack")
